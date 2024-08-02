f5b3f3cbaff43276bc8073b9ad97ad8fc638f673
Asap-Gorbe <dmb_plata@outlook.com>
2024-08-02 19:02:06 +0330

    Update Gorbe coin echo.py

--- Gorbe coin echo.py
+++ Gorbe coin echo.py
@@ -1,30 +1,23 @@
 from typing import Final
 from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
-
 Token: Final = '6513294858:AAHoS4Fb5Lxy9Bj-TfVqGLHqAwpdikbvi2w'
 Bot_user_name: Final = '@Gorbe_Coin_bot'
-
-
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('wellcome this gorbe coin پاره شدم تا همین بیاد')
 def handle_response(text: str) ->str:
+    proccesd_text :str = text.lower()
+    if 'hello' in proccesd_text:
+        return ('Hey!!')
+    else:
+        return (str(text))
+
+    async def Command_Handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        message_type: str = update.message.chat.type
+
     import logging
     from telegram import Update
     from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
-    # Set up logging
-    logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
-
-    # Your Telegram bot token (replace with your actual token)
-    TOKEN = '6513294858:AAHoS4Fb5Lxy9Bj-TfVqGLHqAwpdikbvi2w'
-
-    def start(update: Update, context: CallbackContext) -> None:
-        update.message.reply_text("Hi! I'm your echo bot. Send me any message, and I'll echo it back!")
-
-    def echo(update: Update, context: CallbackContext) -> None:
-        update.message.reply_text(update.message.text)
-
     def main() -> None:
         updater = Updater(TOKEN)
 
@@ -34,9 +27,8 @@ def handle_response(text: str) ->str:
 
         updater.start_polling()
         updater.idle()
-
 if __name__ == '__main__' :
-    print('start polling')
+    print(' we are on mother fucker')
     app = Application.builder().token(Token).build()
     app.add_handler(CommandHandler('start',start_command))
     #app.add_handler(CommandHandler('echo', echo))
