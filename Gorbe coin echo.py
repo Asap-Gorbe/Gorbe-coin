71324593b5157b7b2e8668f14352db9852d9e6e3
Asap-Gorbe <dmb_plata@outlook.com>
2024-08-05 15:21:01 +0330

    Add files via upload
    echo option added

--- Gorbe coin echo.py
+++ Gorbe coin echo.py
@@ -1,23 +1,33 @@
-from typing import Final
-from telegram import Update
-from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes,CallbackContext
-Token: Final = '6513294858:AAHoS4Fb5Lxy9Bj-TfVqGLHqAwpdikbvi2w'
+from telegram import Update, ForceReply
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
-Bot_user_name: Final = '@Gorbe_Coin_bot'
+Token = '6513294858:AAHoS4Fb5Lxy9Bj-TfVqGLHqAwpdikbvi2w'
+Bot_user_name = '@Gorbe_Coin_bot'
 
 async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text('بقیه همینم ندارن')
 
+async def hello_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text('Hi!')
 
-def main() -> None:
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+    def main() -> None:
         updater = Updater(TOKEN)
         dispatcher = updater.dispatcher
         dispatcher.add_handler(CommandHandler("start", start))
+        dispatcher.add_handler(CommandHandler('Hello', Hello))
         updater.start_polling()
         updater.idle()
 
-if __name__ == '__main__' :
+
+if __name__ == '__main__':
     print(' we are on ...')
     app = Application.builder().token(Token).build()
-    app.add_handler(CommandHandler('start',start_command))
+    app.add_handler(CommandHandler('start', start_command))
+    app.add_handler(CommandHandler('Hello', hello_command))
+    app.add_handler(CommandHandler('echo', echo))
+
     app.run_polling(poll_interval=1)
